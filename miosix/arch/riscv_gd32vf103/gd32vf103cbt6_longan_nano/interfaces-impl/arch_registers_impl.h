45805982a5419fcf1d5c0d7a3a092224b9aae2f0
Pasquale Scalise <pasquale.scalise@mail.polimi.it>
2023-06-28 16:06:30 +0200

    Fixed small bugs - NMSIS must be imported in arch_registers_impl

--- miosix/arch/riscv_gd32vf103/gd32vf103cbt6_longan_nano/interfaces-impl/arch_registers_impl.h
+++ miosix/arch/riscv_gd32vf103/gd32vf103cbt6_longan_nano/interfaces-impl/arch_registers_impl.h
@@ -2,9 +2,4 @@
 #ifndef ARCH_REGISTERS_IMPL_H
 #define	ARCH_REGISTERS_IMPL_H
 
-#define GD32VF103C_START
-
-#include "NMSIS/Device/GigaDevice/GD32VF103/Firmware/GD32VF103_standard_peripheral/Include/system_gd32vf103.h"
-#include "NMSIS/Device/GigaDevice/GD32VF103/Firmware/GD32VF103_standard_peripheral/Include/gd32vf103.h"
-
 #endif	//ARCH_REGISTERS_IMPL_H
